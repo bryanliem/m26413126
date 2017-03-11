11cbc937036a24159a4a569d734fa0cb4b50daf7
bryan <m26413126@john.petra.ac.id>
2017-03-11 21:53:42 +0700

    latihan

--- tryphy.py
+++ tryphy.py
@@ -1,11 +1,5 @@
 #/usr/bin/python
-#include<stdio.h>
 
-i = 3
-  if i < 3:
- print ' less than 3 '
-  elif < 5:
- print ' less than 5 '
-  else:
- print ' 5 or more '
-
+umur = 22
+print(umur)
+type(umur)
